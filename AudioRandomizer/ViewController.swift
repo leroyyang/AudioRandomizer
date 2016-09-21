5963ccaf87916701e05714c88a9d5582cad4781f
LiWei Yang <leroyyang@gmail.com>
2016-09-21 14:53:08 +0800

    Initial Commit

---
+++ AudioRandomizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AudioRandomizer
+//
+//  Created by leroy on 2016/9/21.
+//  Copyright © 2016年 leroy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
